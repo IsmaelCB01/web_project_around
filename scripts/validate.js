05a973270de4083a8b36d03a89d0c88c08a8e434
Ismael <ismaelcruzbustillos@gmail.com>
2025-02-07 23:18:58 -0600

    bug: Correccion de clase al abrir popup de imagen en grande

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,3 +1,4 @@
+//Selectores de HTML guardados en un objeto
 export const settings = {
   formSelector: ".form",
   inputSelector: ".form__input",
@@ -7,6 +8,7 @@ export const settings = {
   errorClass: "form__span-error-visible",
 };
 
+//funcion para mostrar los errores de la propiedada validation de los input
 const showInputError = (formElement, inputElement, settings) => {
   const errorMessage = inputElement.validationMessage;
   const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
@@ -15,6 +17,7 @@ const showInputError = (formElement, inputElement, settings) => {
   errorElement.classList.add(settings.errorClass);
 };
 
+//funcion para ocultar los errores de la propiedad validation de los input
 const hideInputError = (formElement, inputElement, settings) => {
   const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
   inputElement.classList.remove(settings.inputErrorClass);
@@ -22,6 +25,7 @@ const hideInputError = (formElement, inputElement, settings) => {
   errorElement.textContent = "";
 };
 
+//funcion para validar los inputs y asignarles funciones de error o ocultarlas
 const checkInputValidity = (formElement, inputElement, settings) => {
   if (!inputElement.validity.valid) {
     showInputError(formElement, inputElement, settings);
@@ -30,12 +34,14 @@ const checkInputValidity = (formElement, inputElement, settings) => {
   }
 };
 
+//funcion que valida de una lista de Arrays de inputs si cumplen con sus validaciones, si al menos uno no corresponde devuelve true la funcion
 const hasInvalidInput = (inputList) => {
   return inputList.some((inputElement) => {
     return !inputElement.validity.valid;
   });
 };
 
+//funcion que llama a la validacion de la lista de los inputs para habilitar o desabilitar el boton de guardado del formulario
 const toggleButtonState = (inputList, buttonElement, settings) => {
   if (hasInvalidInput(inputList)) {
     buttonElement.classList.add(settings.inactiveButtonClass);
@@ -46,6 +52,7 @@ const toggleButtonState = (inputList, buttonElement, settings) => {
   }
 };
 
+//funcion que establece los handlers a la lista de inputs y botones
 const setEventListeners = (formElement, settings) => {
   const inputList = Array.from(
     formElement.querySelectorAll(settings.inputSelector)
@@ -63,6 +70,7 @@ const setEventListeners = (formElement, settings) => {
   });
 };
 
+//funcion que llama a todos los formularios
 function enableValidation(settings) {
   const formList = Array.from(document.querySelectorAll(settings.formSelector));
   formList.forEach((formElement) => {
